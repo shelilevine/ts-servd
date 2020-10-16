930403adadc91d2bb9fc9564ed893f45be9f7860
shelilevine <shelisteinberg@gmail.com>
2020-10-16 11:34:57 -0400

    refactor: interface for set search results

--- src/interfaces/index.js
+++ src/interfaces/index.js
@@ -1,5 +1,6 @@
 import Recipe from "./Recipe";
 import UpdateRecipe from "./updateRecipe";
 import User from "./user";
+import SetSearch from "./setSearch";
 
-export { Recipe, UpdateRecipe, User };
+export { Recipe, UpdateRecipe, User, SetSearch };
